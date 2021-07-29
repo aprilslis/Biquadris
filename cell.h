5511b9e71580c88805e98c04f09e6488250fa488
SrijanChaudhuri <76275146+SrijanChaudhuri@users.noreply.github.com>
2021-07-29 12:45:28 +0400

    Create cell.h

---
+++ cell.h
@@ -0,0 +1,23 @@
+#ifndef _CELL_H_
+#define _CELL_H_
+
+class Cell {
+
+        char type;
+        int age;
+        int row;
+        int col;
+
+        public:
+        Cell(int row, int col);
+        void setType(char type);
+        char getType();
+        int getAge();
+        int getRow();
+        int getCol();
+        bool isFull();
+        void updateAge();
+
+};
+
+#endif
