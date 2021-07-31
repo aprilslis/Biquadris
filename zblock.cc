a9372a24aca8f08829739b8a0de9ff273fe71d97
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-07-31 15:05:06 -0400

    Create zblock.cc

---
+++ zblock.cc
@@ -0,0 +1,142 @@
+#include "zblock.h"
+
+void ZBlock::switchBlocks(vector<Cell *> other) {
+	// emptying block temporarily
+	for (int i = 0; i < size; i++) { 
+	        block[i]->setType('\0'); 
+	}
+
+	// asserting invariants
+	for (int i = 0; i < size; i++) {
+	    if (other[i]->isFull()) {
+	    	// rewinding block
+	        for (int j = 0; j < size; j++) {
+	                block[j]->setType('Z');
+	         }
+	         throw InvalidMoveException e{};
+	    }
+	} 
+
+	//doing the switch 
+	block.clear();
+	block = other;
+	for (int i = 0; i < size; i++) {
+	        block[i]->setType('Z'); 
+	}
+	lowerleft = other[0];
+}
+
+void ZBlock::init(std::vector<std::vector<Cell *>> g) {
+	grid = g;
+	pos = 1;
+	size = 4;
+	lost();
+	lowerleft = grid[3][1];
+	block.push_back(lowerleft);
+	block.push_back(grid[3][2]);
+	block.push_back(grid[2][0]);
+	block.push_back(grid[2][1]);
+	for (int i = 0; i < size; i++) {
+		block[i]->setType('Z');
+	}
+}
+
+
+
+void ZBlock::lost() {
+	if (grid[3][1]->isFull() || grid[3][2]->isFull() || grid[2][0]->isFull() || grid[2][1]->isFull()) {
+		throw LostException();
+	}
+}
+
+void ZBlock::moveLeft() {
+	for (int i = 0; i < size; i++) {
+		if (block[i]->getCol() - 1 < 0) {
+			throw InvalidMoveException e{};
+		}
+	}
+
+	vector<Cell *> temp;
+	for (int i = 0; i < size; i++) {
+		int row = block[i]->getRow();
+		int col = block[i]->getCol();
+		temp.push_back(grid[row][col - 1]);
+	}
+
+	switchBlocks(temp);
+}
+
+void ZBlock::moveRight() {
+	for (int i = 0; i < size; i++) {
+		if (block[i]->getCol() + 1 > 10) {
+			throw InvalidMoveException e{};
+		}
+	}
+
+	vector<Cell *> temp;
+	for (int i = 0; i < size; i++) {
+		int row = block[i]->getRow();
+		int col = block[i]->getCol();
+		temp.push_back(grid[row][col + 1]);
+	}
+
+	switchBlocks(temp);
+}
+
+void ZBlock::moveDown() {
+	for (int i = 0; i < size; i++) {
+		if (block[i]->getRow() + 1 > 17) {
+			throw InvalidMoveException e{}; 
+		}
+	}
+
+	vector<Cell *> temp;
+	for (int i = 0; i < size; i++) {
+		int row = block[i]->getRow();
+		int col = block[i]->getCol();
+		temp.push_back(grid[row + 1][col]);
+	}
+
+	switchBlocks(temp);
+}
+
+void ZBlock::drop() {
+	try {
+		while(true) {
+			moveDown();
+		}
+	} catch (InvalidMoveException &e) {
+		break;
+	}
+}
+
+void ZBlock::rotateCW() {
+	vector<Cell *> temp;
+	int row = lowerleft->getRow;
+	int col = lowerleft->getCol;
+	if (pos == 1) {
+		if (row - 1 < 0 || row - 2 < 0 || col - 1 < 0) {
+			throw InvalidMoveException e{};
+		}
+        temp.push_back(grid[row][col - 1]);
+        temp.push_back(grid[row - 1][col]);
+		temp.push_back(grid[row - 1][col - 1]);
+        temp.push_back(grid[row - 2][col]);
+		switchBlocks(temp);
+		++pos;
+	} else if (pos == 2) {
+		if (row - 1 < 0 || col + 1 > 10 || col + 2 > 10) {
+			throw InvalidMoveException e{};
+		}
+		temp.push_back(grid[row][col + 1]);
+		temp.push_back(grid[row][col + 2]);
+		temp.push_back(grid[row - 1][col]);
+		temp.push_back(grid[row - 1][col + 1]);
+		switchBlocks(temp);
+	    --pos;
+	}
+}
+
+void ZBlock::rotateCCW() {  // CCW and CW have same effect as there are 2 positions in ZBlock
+	rotationCW();
+}
