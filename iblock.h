da1615a3079ae30d9df7e2525b69fb296e1cd268
SrijanChaudhuri <76275146+SrijanChaudhuri@users.noreply.github.com>
2021-07-31 14:21:49 +0400

    Create iblock.h

---
+++ iblock.h
@@ -0,0 +1,23 @@
+#ifndef _IBLOCK_H_
+#define _IBLOCK_H_
+
+#include "block.h"
+
+class IBlock : public Block {
+  
+    int pos;
+    int size;
+    void switchBlocks(std::vector<Cell *> other);
+  
+    public:
+        void init(std::vector<std::vector<Cell *>> g) override;
+        void moveLeft() override;
+        void moveRight() override;
+        void moveDown() override;
+        void drop() override;
+        void rotationCW() override;
+        void rotationCCW() override;
+  
+};
+
+#endif
