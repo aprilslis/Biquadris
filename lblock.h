ce72fbce3b871c55a6b5d9417cee73671afcb981
SrijanChaudhuri <76275146+SrijanChaudhuri@users.noreply.github.com>
2021-08-01 12:11:27 +0400

    Create lblock.h

---
+++ lblock.h
@@ -0,0 +1,24 @@
+#ifndef _LBLOCK_H_
+#define _LBLOCK_H_
+
+#include "block.h"
+
+class LBlock : public Block {
+  
+    int pos;
+    int size;
+    void switchBlocks(std::vector<Cell *> other);
+  
+    public:
+        void init(std::vector<std::vector<Cell *>> g) override;
+        void lost() override;
+        void moveLeft() override;
+        void moveRight() override;
+        void moveDown() override;
+        void drop() override;
+        void rotationCW() override;
+        void rotationCCW() override;
+  
+};
+
+#endif
