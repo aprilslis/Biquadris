5db279faa86bc4d40d43e007408f94cacecf611e
SrijanChaudhuri <76275146+SrijanChaudhuri@users.noreply.github.com>
2021-08-02 15:34:15 +0400

    Create level1.cc

---
+++ level1.cc
@@ -0,0 +1,54 @@
+#include "level0.h" 
+
+using namespace std;
+
+void Level0::init(string sequence) {
+	this->sequence = sequence;
+	len = sequence.length();
+	pos = 0;
+}
+
+Block * Level0::generateBlock() {
+	if (pos + 1 > len) {
+		pos = 0;
+	}
+	switch(sequence[pos]) {
+		case 'I' : 
+			++pos;
+			return new IBlock{};
+		case 'J' :
+			++pos;
+			return new JBlock{};
+		case 'L' :
+			++pos;
+			return new LBlock{};
+		case 'O' :
+			++pos;
+			return new OBlock{};
+		case 'S' :
+			++pos;
+			return new SBlock{};
+		case 'Z' :
+			++pos;
+			return new ZBlock{};
+		case 'T' :
+			++pos;
+			return new TBlock{};
+		default :
+			++pos;
+			return nullptr;
+	}
+}
+
+void Level0::setSeed() {
+}
+
+int Level0::getLevel() {
+	return 0;
+}
+
+
+
+
+
+
