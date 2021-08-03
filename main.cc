afd20c548face39304bb1686e66641c9c1fa8939
aprilslis <s826li@uwaterloo.ca>
2021-08-03 11:49:24 -0400

    added stuff in main

--- main.cc
+++ main.cc
@@ -12,6 +12,21 @@ using namespace std;
 
 
 int main(){
+    
     Grid g;
+    g.printGrid();
+    cout<<"---------------------------------------"<<endl;
+
+    IBlock *i = new IBlock{};
+    JBlock *j = new JBlock{};
+    LBlock *l = new LBlock{};
+    OBlock *o = new OBlock{};
+    TBlock *t = new TBlock{};
+    ZBlock *z = new ZBlock{};
+    SBlock *s = new SBlock{};
+    
+    g.addBlock(i);
+    i->moveRight();
+    i->drop();
     g.printGrid();
 }
