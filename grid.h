54c45224406ae6ce97aa4824391dcb0273f9865a
SrijanChaudhuri <76275146+SrijanChaudhuri@users.noreply.github.com>
2021-08-03 13:21:35 +0400

    Update grid.h

--- grid.h
+++ grid.h
@@ -16,20 +16,23 @@ class Grid{
 
   public:
     void Grid();
-    void printGrid(); //print out current board
-    void init();
-    bool won(); //check if game has ended
-    void clearBoard(); //release all existing cells
-    Cell *getCell(int x, int y); //get cell at x,y
     
-    int findFullRows(); //check for completed rows, return number of rows cleared
-    void updateBoard(int y); //used after clearing row/block, let all block fall over gravity(?)
+    Cell *getCell(int x, int y); // get cell at (row, column)  
+    void addBlock(Block *b); // add a new given block at left top corner
+    void replaceBlock(Block *b); // replace current block with new block b
+    
+    bool isFullRow(); // checks if row is full
+    int countFullRows(); // returns number of full rows
+    void clearFullRows(); // clears the full rows one at a time
+    void updateRows(int y); // used after clearing row/block, let all block fall over gravity(?)
 
-    void addBlock(Block *b); //add a new given block at left top corner
-    void replaceBlock(Block *b); //replace current block with new block b
-
-    void levelUp(); //level game up
-    void levelDown(); //level game down
+    void levelUp(); // level game up
+    void levelDown(); // level game down
+    
+    void clearGrid(); // resets the grid
+    void printGrid(); // print out current board
+    
+    void ~Grid();
 
 };
 
