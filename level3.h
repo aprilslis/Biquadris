0f18df5f80f1f07840ceb12fff06e342b718ef85
aprilslis <s826li@uwaterloo.ca>
2021-08-05 16:34:40 -0400

    added files level3,4 (did not write anythign in it)

---
+++ level3.h
@@ -0,0 +1,11 @@
+#ifndef _LEVEL3_H_
+#define _LEVEL3_H_
+
+#include "level.h"
+#include <string>
+
+class Level3: public Level {
+	
+};
+
+#endif
---
+++ level4.h
@@ -0,0 +1,11 @@
+#ifndef _LEVEL4_H_
+#define _LEVEL4_H_
+
+#include "level.h"
+#include <string>
+
+class Level4: public Level {
+	
+};
+
+#endif
