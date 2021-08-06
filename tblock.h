51128f4369a56e5cdaa211fec665c4d27274ff4e
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-08-06 11:07:14 -0400

    Update tblock.h

--- tblock.h
+++ tblock.h
@@ -10,7 +10,7 @@ class TBlock : public Block {
     void switchBlocks(std::vector<Cell *> other);
   
     public:
-        TBlock();
+        TBlock(int level);
         void init(std::vector<std::vector<Cell *>> &g) override;
         void lost() override;
         void moveLeft() override;
