8efdbe177eea0c5981eeae8c585312de20c5f80a
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-08-06 11:04:22 -0400

    Update oblock.h

--- oblock.h
+++ oblock.h
@@ -10,6 +10,7 @@ class OBlock : public Block {
     void switchBlocks(std::vector<Cell *> other);
   
     public:
+        OBlock();
         void init(std::vector<std::vector<Cell *>> &g) override;
         void lost() override;
         void moveLeft() override;
