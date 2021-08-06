3f4b7702503cfd76fc6214203b35291bb3f6b312
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-08-06 11:06:47 -0400

    Update lblock.h

--- lblock.h
+++ lblock.h
@@ -10,7 +10,7 @@ class LBlock : public Block {
     void switchBlocks(std::vector<Cell *> other);
   
     public:
-        LBlock();
+        LBlock(int level);
         void init(std::vector<std::vector<Cell *>> &g) override;
         void lost() override;
         void moveLeft() override;
