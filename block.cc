76429d8c10c6e46a7fdfc4f63615793759bba4f4
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-08-06 11:11:20 -0400

    Update block.cc

--- block.cc
+++ block.cc
@@ -4,6 +4,8 @@ Block::Block(int level) {
 	this->level = level;
 }
 
+virtual ~Block() {};
+
 void Block::emptyBlock() {
   for (int i = 0; i < (int)block.size(); i++) {
     block[i]->setType('\0');
