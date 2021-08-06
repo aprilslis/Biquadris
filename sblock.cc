4dcdfc059db7209b4abdf5d1ff5c2be59faedaca
SrijanChaudhuri <76275146+SrijanChaudhuri@users.noreply.github.com>
2021-08-06 14:15:13 +0400

    Update sblock.cc

--- sblock.cc
+++ sblock.cc
@@ -5,6 +5,7 @@ void SBlock::switchBlocks(vector<Cell *> other) {
 	// emptying block temporarily
 	for (int i = 0; i < size; i++) { 
 	        block[i]->setType('\0'); 
+		block[i]->setIdentity(0);
 	}
 
 	// asserting invariants
@@ -13,6 +14,7 @@ void SBlock::switchBlocks(vector<Cell *> other) {
 	    	// rewinding block
 	        for (int j = 0; j < size; j++) {
 	                block[j]->setType('S');
+			block[j]->setIdentity(identity);
 	         }
 	         throw InvalidMoveException();
 	    }
@@ -23,6 +25,7 @@ void SBlock::switchBlocks(vector<Cell *> other) {
 	block = other;
 	for (int i = 0; i < size; i++) {
 	        block[i]->setType('S'); 
+		block[i]->setIdentity(identity);
 	}
 	lowerleft = other[0];
 }
@@ -39,6 +42,7 @@ void SBlock::init(std::vector<std::vector<Cell *>> &g) {
 	block.push_back(grid[2][2]);
 	for (int i = 0; i < size; i++) {
 		block[i]->setType('S');
+		block[i]->setIdentity(identity);
 	}
 }
 
