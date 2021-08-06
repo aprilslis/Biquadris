8fb6f845e5eecf9229d08479363912ccaa4fe76f
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-08-06 12:00:31 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -15,6 +15,41 @@ using namespace std;
 int main(int argc, char *argv[]){
     Game g;
     g.start();
+    for (int i = 1; i < argc; i++){
+        string cmdin;
+        cmdin = {argv[i]};
+        if (cmdin == "-text") {
+            displayTextOnly(); 
+        } 
+        else if (cmdin == "-seed") {
+            istringstream ss(argv[i++]);
+            int seedin;
+            ss >> seedin;
+            if (seed > 0){
+                setSeed(seedin);
+                srand(seed);
+            }
+        } else if (cmdin == "-scriptfile1") {
+            istringstream ss(argv[i++]); 
+            string fname; 
+            ss >> fname; 
+            setScript1(fname); 
+        } else if (cmdin == "-scriptfile2") {
+            istringstream ss(argv[i++]); 
+            string fname; 
+            ss >> fname; 
+            setScript2(fname);    
+        }
+        else if (cmdin == "-startlevel") {
+            int lvlin;
+            istringstream ss(argv[i++]);
+            ss >> lvlin; 
+            if (lvlin >= 0 && lvlin <= 4)  {
+                setStartLevel(lvlin); 
+            }
+        }
+    }
+
     // testing:
     /*Grid g;
     g.printGrid();
