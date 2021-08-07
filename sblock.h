6c711883e67e845ee7a6b58e6b3adc9f43b67251
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-08-07 14:42:40 -0400

    Update sblock.h

--- sblock.h
+++ sblock.h
@@ -12,6 +12,7 @@ class SBlock : public Block {
   
     public:
         SBlock(int level);
+	char getType();
         void init(std::vector<std::vector<Cell *>> &g) override;
         void lost() override;
         void moveLeft() override;
