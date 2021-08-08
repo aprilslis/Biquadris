d446e4c259a82a95b8deab05925f7f1e9ed5d65c
aprilslis <s826li@uwaterloo.ca>
2021-08-08 16:42:33 -0400

    got rid of all .d .o

--- main.d
+++
@@ -1,3 +0,0 @@
-main.o: main.cc game.h grid.h cell.h level.h iblock.h block.h jblock.h \
- lblock.h oblock.h sblock.h zblock.h tblock.h level0.h level1.h level2.h \
- level3.h level4.h score.h display.h textdisplay.h
