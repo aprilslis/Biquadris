a7e524fc1b76c468f8fc8ddab8eb9c8b3090a3b7
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-08-12 16:38:27 -0400

    Update level1.h

--- level1.h
+++ level1.h
@@ -5,7 +5,7 @@
 
 class Level1: public Level {
 	public:
-	Block * generateRandomBlock(int seed = 0) override;
+	Block * generateRandomBlock(int seed = 0, int id) override;
 	int getLevel() override;
 };
 
