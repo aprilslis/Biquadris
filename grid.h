6120d4123c0e730b34ef26e8a9fa5677c8ab0c56
Rainbowli8 <68574157+Rainbowli8@users.noreply.github.com>
2021-08-13 15:25:24 -0400

    Update grid.h

--- grid.h
+++ grid.h
@@ -72,7 +72,8 @@ public:
 
     void updateScore(); //update current score of game
     int getScore();     //get current score of game
-
+    int getHighScore(); //get current highest score of game
+    
     void levelUp();          // level game up
     void levelDown();        // level game down
     void setLevel();         //update the level
