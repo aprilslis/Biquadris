a19e40e98f3d07aeb045452ee2e14c8b6567d6cd
s826li <s826li@student.cs.uwaterloo.ca>
2021-08-13 18:35:26 -0400

    update test case

--- textdisplay.d
+++
@@ -1,4 +0,0 @@
-textdisplay.o: textdisplay.cc textdisplay.h basedisplay.h grid.h cell.h \
- level.h iblock.h block.h jblock.h lblock.h oblock.h sblock.h zblock.h \
- tblock.h level0.h level1.h level2.h level3.h level4.h starblock.h \
- score.h
