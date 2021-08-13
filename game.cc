f7c09899a6f3f2d08cbbfb4f33eabbf466c4e10c
s826li <s826li@student.cs.uwaterloo.ca>
2021-08-13 18:37:17 -0400

    put graphic display back

--- game.cc
+++ game.cc
@@ -9,7 +9,7 @@ board1{file1,startlevel}
 ,onlyText{false}
 ,useSeqFile{false}
 ,textdisplay{&board1,&board2}
-//,graphicdisplay{&board1,&board2} 
+,graphicdisplay{&board1,&board2} 
 {
     
 }
@@ -209,7 +209,7 @@ void Game::start(){
                     
 
                     textdisplay.setBlind(0);
-                    //graphicdisplay.setBlind(0);
+                    graphicdisplay.setBlind(0);
 
                     if(curNum==1){
                         cur = &board2;
@@ -368,11 +368,11 @@ void Game::specialEffects(int curNum){//curNum is the opponent's number
             cout<<endl<<"**Blind** effect would be applied to Player "<<curNum<<endl;
             if(curNum==1){
                 textdisplay.setBlind(1);
-                //graphicdisplay.setBlind(1);
+                graphicdisplay.setBlind(1);
             }
             else{
                 textdisplay.setBlind(2);
-                //graphicdisplay.setBlind(2);
+                graphicdisplay.setBlind(2);
             }
         }
         else if(cmpString(input,"heavy")){
@@ -430,7 +430,7 @@ void Game::restart(){
     board2.clearGrid();
     useSeqFile = false;
     textdisplay.setBlind(0);
-    //graphicdisplay.setBlind(0);
+    graphicdisplay.setBlind(0);
 
     start();
 }
@@ -491,7 +491,7 @@ void Game::drawText(){
 }
 
 void Game::drawGraphic(){
-    //graphicdisplay.printDisplay();
+    graphicdisplay.printDisplay();
 }
 
 void Game::draw(int multiplier){
